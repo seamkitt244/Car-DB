f35f8a39c8cecefe0e42496bf8f9b7956d20dbed
Tony Theodoropoulos <theodot@outlook.com>
2020-02-14 13:46:09 -0500

    Add project files.

---
+++ CarInventory/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarInventory
+{
+    public partial class Form1 : Form
+    {
+        List<Car> carsList = new List<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string year, make, colour, mileage;
+
+            year = yearInput.Text;
+            make = makeInput.Text;
+            colour = colourInput.Text;
+            mileage = mileageInput.Text;
+
+            Car car = new Car(year, make, colour, mileage);
+
+            carsList.Add(car);
+
+            outputLabel.Text = "";
+
+            //for (int i = 0; i < carsList.Count; i++)
+            //{
+            //    outputLabel.Text += carsList[i].year + " " + carsList[i].make + " " + carsList[i].colour + " " + carsList[i].mileage + "\n";
+            //}
+
+            foreach(Car c in carsList)
+            {
+                outputLabel.Text += c.year + " "
+                    + c.make + " "
+                    + c.colour + " "
+                    + c.mileage + "\n";
+            }
+        }
+    }
+}
